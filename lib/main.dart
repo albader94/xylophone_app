ac70f844a06a89a5ed2955a1f25444581ebb806c
Bader Albader <b.albader94@gmail.com>
2021-09-06 21:49:15 +0300

    Layout and functionality
    Plays notes on each click on each Xylophone button

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(XylophoneApp());
 }
 
 class XylophoneApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        body: Column(
+          children: <Widget>[
+            xylophoneButton(1, Colors.red),
+            xylophoneButton(2, Colors.orange),
+            xylophoneButton(3, Colors.yellow),
+            xylophoneButton(4, Colors.green),
+            xylophoneButton(5, Colors.teal),
+            xylophoneButton(6, Colors.blue),
+            xylophoneButton(7, Colors.purple),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget xylophoneButton(int fileNum, Color color) {
+  return  Expanded(
+    child: MaterialButton(
+      color: color,
+      minWidth: double.infinity,
+      onPressed: (() {
+        final player = AudioCache();
+        player.play('note$fileNum.wav');
+      }),
+    ),
+  );
 }
